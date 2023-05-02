742cb11742fe2c2ac1887237bf300ea123d613e1
Jan <goarano@gmail.com>
2023-05-02 12:54:10 +0200

    removed prompt to install fzf

--- packagemanager-fzf.plugin.zsh
+++ packagemanager-fzf.plugin.zsh
@@ -1,19 +1,19 @@
-if ! [ $commands[fzf] ]; then
-    echo 'zsh-fzf-packagemanager: fzf command not found, please install https://github.com/junegunn/fzf' >&2
-else
-    if [ $commands[apt-get] ]; then
-        source "$(dirname $0)/apt.zsh"
-        __pmf_apt_setup
-        unfunction __pmf_apt_setup
-    fi
-    if [ $commands[brew] ]; then
-        source "$(dirname $0)/brew.zsh"
-        __pmf_brew_setup
-        unfunction __pmf_brew_setup
-    fi
-    if [ $commands[dnf] ]; then
-        source "$(dirname $0)/dnf.zsh"
-        __pmf_dnf_setup
-        unfunction __pmf_dnf_setup
-    fi
+#!/usr/bin/env zsh
+
+if [ $commands[apt-get] ]; then
+    source "$(dirname $0)/apt.zsh"
+    __pmf_apt_setup
+    unfunction __pmf_apt_setup
+fi
+
+if [ $commands[brew] ]; then
+    source "$(dirname $0)/brew.zsh"
+    __pmf_brew_setup
+    unfunction __pmf_brew_setup
+fi
+
+if [ $commands[dnf] ]; then
+    source "$(dirname $0)/dnf.zsh"
+    __pmf_dnf_setup
+    unfunction __pmf_dnf_setup
 fi
